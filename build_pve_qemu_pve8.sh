d9714f4713a78dae89b67acc6c6c855ed886c679
July's <37677092+july929@users.noreply.github.com>
2026-01-04 11:07:55 +0800

    Update build_pve_qemu_pve8.sh

--- build_pve_qemu_pve8.sh
+++ build_pve_qemu_pve8.sh
@@ -4,15 +4,16 @@ ls
 df -h
 git clone git://git.proxmox.com/git/pve-qemu.git
 cd pve-qemu
-git reset --hard 245689b9ae4120994de29b71595ea58abac06f3c # 9.2.0-7
+# 9.2.0-7
+git reset --hard 245689b9ae4120994de29b71595ea58abac06f3c 
 apt install devscripts -y
 mk-build-deps --install
 git submodule update --init --recursive
-cp ../sedPatch-pve-qemu-kvm7-8-anti-dection.sh qemu/
+cp ../sedPatch-pve-qemu-kvm9-10-anti-dection.sh qemu/
 cd qemu
 meson subprojects download
-chmod +x sedPatch-pve-qemu-kvm7-8-anti-dection.sh
-bash sedPatch-pve-qemu-kvm7-8-anti-dection.sh
+chmod +x sedPatch-pve-qemu-kvm9-10-anti-dection.sh
+bash sedPatch-pve-qemu-kvm9-10-anti-dection.sh
 cp ../../smbios.h include/hw/firmware/smbios.h
 cp ../../smbios.c hw/smbios/smbios.c
 sed -i 's/!object_dynamic_cast/object_dynamic_cast/g' hw/vfio/igd.c
