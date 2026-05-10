a7310625da8c5136069444b8bba8ba783644e149
July's <37677092+july929@users.noreply.github.com>
2026-05-10 23:56:30 +0800

    Update sedPatch-pve-qemu-kvm11-anti-dection.sh

--- sedPatch-pve-qemu-kvm11-anti-dection.sh
+++ sedPatch-pve-qemu-kvm11-anti-dection.sh
@@ -94,8 +94,8 @@ sed -i 's/"QEMU/"'${brand}'/g' hw/usb/dev-wacom.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/usb/u2f-emulated.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/usb/u2f-passthru.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/usb/u2f.c
-sed -i 's/"BOCHS/"INTEL /g' include/hw/acpi/aml-build.h
-sed -i 's/"BXPC/"U Rvp   /g' include/hw/acpi/aml-build.h
+sed -i 's/"BOCHS/"INTEL/g' include/hw/acpi/aml-build.h
+sed -i 's/"BXPC /"U Rvp/g' include/hw/acpi/aml-build.h
 sed -i 's/"QEMU0002/"'${brand}'0002/g' include/standard-headers/linux/qemu_fw_cfg.h
 sed -i 's/0x51454d5520434647ULL/0x51434f4d20434647ULL/g' include/standard-headers/linux/qemu_fw_cfg.h
 sed -i 's/"QEMU/"'${brand}'/g' migration/migration.c
@@ -138,8 +138,8 @@ sed -i 's/lat = 0xfff/lat = 0x1fff/g' hw/i386/acpi-build.c  # A value > 100 indi
 sed -i 's/"WAET"/"WWWT"/g' hw/i386/acpi-build.c # "WAET" is also present as a string inside the WAET table, so there's no need to check for its table signature
 sed -i 's/rev = 1/rev = 3/g' hw/i386/acpi-build.c # 全部升级最低为3
 sed -i 's/dev = aml_device("PCI0");/aml_append(sb_scope, aml_name_decl("OSYS", aml_int(0x03E8)));\n\tAml *osi = aml_if(aml_equal(aml_call1("_OSI", aml_string("Windows 2012")), aml_int(1)));\n\taml_append(osi, aml_store(aml_int(0x07DC), aml_name("OSYS")));\n\taml_append(sb_scope, osi);\n\tosi = aml_if(aml_equal(aml_call1("_OSI",aml_string("Windows 2013")), aml_int(1)));\n\taml_append(osi, aml_store(aml_int(0x07DD), aml_name("OSYS")));\n\taml_append(sb_scope, osi);\n\taml_append(sb_scope, aml_name_decl("_TZ", aml_int(0x03E8)));\n\taml_append(sb_scope, aml_name_decl("_PTS", aml_int(0x03E8)));\n\tdev = aml_device("PCI0");/g' hw/i386/acpi-build.c # windows 2012 2013 _TZ_ _PTS dsdt
-#sed -i 's/0x0627/0x046D/g' hw/input/virtio-input-hid.c # 0x0627=QEMU tablet usb键鼠会有少量问题
+sed -i 's/0x0627/0x046D/g' hw/input/virtio-input-hid.c # 0x0627=QEMU tablet usb键鼠会有少量问题
 sed -i 's/0x0627/0x046D/g' hw/usb/dev-hid.c # 0x0627=QEMU tablet usb键鼠会有少量问题
-#sed -i 's/0x0001/0xC31C,/g' hw/input/virtio-input-hid.c # 0x0627=QEMU tablet usb键鼠会有少量问题
-sed -i 's/0x0001/0xC31C,/g' hw/usb/dev-hid.c # 0x0627=QEMU tablet usb键鼠会有少量问题
+sed -i 's/0x0001/0xC31C/g' hw/input/virtio-input-hid.c # 0x0627=QEMU tablet usb键鼠会有少量问题
+sed -i 's/0x0001/0xC31C/g' hw/usb/dev-hid.c # 0x0627=QEMU tablet usb键鼠会有少量问题
 echo "结束sed工作"
