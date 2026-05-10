8b5792414598cb9d0b7c8e8a2f13919d897fba42
July's <37677092+july929@users.noreply.github.com>
2026-05-10 13:58:59 +0800

    Update sedPatch-pve-qemu-kvm11-anti-dection.sh

--- sedPatch-pve-qemu-kvm11-anti-dection.sh
+++ sedPatch-pve-qemu-kvm11-anti-dection.sh
@@ -6,10 +6,10 @@ sed -i 's/QEMU VVFAT", 10/'${brand}' VVFAT", 10/g' block/vvfat.c
 sed -i 's/QEMU Microsoft Mouse/'${brand}' Microsoft Mouse/g' chardev/msmouse.c
 sed -i 's/QEMU Wacom Pen Tablet/'${brand}' Wacom Pen Tablet/g' chardev/wctablet.c
 sed -i 's/QEMU vhost-user-gpu/'${brand}' vhost-user-gpu/g' contrib/vhost-user-gpu/vhost-user-gpu.c
-sed -i 's/build_append_padded_str(array, desc->oem_id, 6, '\''\\0'\'');/build_append_padded_str(array, ACPI_BUILD_APPNAME6, 6, '\''\\0'\'');/' hw/acpi/aml-build.c
-sed -i 's/g_array_append_vals(array, ACPI_BUILD_APPNAME8, 4);/g_array_append_vals(array, "ACPI", 4);/' hw/acpi/aml-build.c
-sed -i 's/build_append_int_noprefix(tbl, 0 \/\* Unspecified \*\/, 1);/build_append_int_noprefix(tbl, 1 \/\* Desktop \*\/, 1);/' hw/acpi/aml-build.c
-sed -i 's/build_append_padded_str(tbl, "QEMU", 8, '\''\\0'\'');/build_append_padded_str(tbl, "", 8, '\''\\0'\'');/' hw/acpi/aml-build.c
+sed -i 's/desc->oem_id/ACPI_BUILD_APPNAME6/g' hw/acpi/aml-build.c
+sed -i 's/desc->oem_table_id/ACPI_BUILD_APPNAME8/g' hw/acpi/aml-build.c
+sed -i 's/array, ACPI_BUILD_APPNAME8/array, "PTL "/g' hw/acpi/aml-build.c
+sed -i 's/"QEMU/"Intel/g' hw/acpi/aml-build.c
 
 grep "do this once" hw/acpi/vmgenid.c >/dev/null
 if [ $? -eq 0 ]; then
@@ -37,18 +37,14 @@ else
 fi
 sed -i 's/"QEMU/"'${brand}'/g' hw/i386/fw_cfg.c
 sed -i 's/"QEMU Virtual CPU/"CPU/g' hw/i386/pc.c
-sed -i 's/pcmc->smbios_defaults = true;/pcmc->smbios_defaults = false;/' hw/i386/pc.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/i386/pc_piix.c
 sed -i 's/Standard PC (i440FX + PIIX, 1996)/'${brand}' M4A88TD-Mi440fx/g' hw/i386/pc_piix.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/i386/pc_q35.c
 sed -i 's/Standard PC (Q35 + ICH9, 2009)/'${brand}' M4A88TD-Mq35/g' hw/i386/pc_q35.c
 sed -i 's/mc->name, pcmc->smbios_legacy_mode,/"'${brand}'-PC", pcmc->smbios_legacy_mode,/g' hw/i386/pc_q35.c
-sed -i 's/padstr8(buf + 8, 8, "QEMU");/padstr8(buf + 8, 8, "Samsung");/' hw/ide/atapi.c
-sed -i 's/padstr8(buf + 16, 16, "QEMU DVD-ROM");/padstr8(buf + 16, 16, "DVD-ROM");/' hw/ide/atapi.c
-sed -i 's/snprintf(s->drive_serial_str, sizeof(s->drive_serial_str), "QM%05d", s->drive_serial);/s->drive_serial_str[0] = '\''\\0'\'';  \/\/ Empty string fallback instead of QEMU default/' hw/ide/core.c
-sed -i 's/strcpy(s->drive_model_str, "QEMU DVD-ROM");/strcpy(s->drive_model_str, "HL-DT-ST BD-RE WH16NS60");/' hw/ide/core.c
-sed -i 's/strcpy(s->drive_model_str, "QEMU MICRODRIVE");/strcpy(s->drive_model_str, "Hitachi HMS360404D5CF00");/' hw/ide/core.c
-sed -i 's/strcpy(s->drive_model_str, "QEMU HARDDISK");/strcpy(s->drive_model_str, "Samsung SSD 980 500GB");/' hw/ide/core.c
+sed -i 's/"QEMU/"'${brand}'/g' hw/ide/atapi.c
+sed -i 's/"QEMU/"'${brand}'/g' hw/ide/core.c
+sed -i 's/qemu_hw_version()/s->drive_serial_str/g' hw/ide/core.c
 sed -i 's/0x09, 0x03, 0x00, 0x64, 0x64, 0x01, 0x00/0x09, 0x03, 0x00, 0x64, 0x64, 0x9a, 0x02/g' hw/ide/core.c  #ide sata 通电时间power on hours改为666小时 0x029a
 sed -i 's/0x0c, 0x03, 0x00, 0x64, 0x64, 0x00, 0x00/0x0c, 0x03, 0x00, 0x64, 0x64, 0x9a, 0x02/g' hw/ide/core.c  #ide sata 通电次数power cycle count改为666次 0x029a
 sed -i 's/"QEMU/"'${brand}'/g' hw/input/adb-kbd.c
@@ -58,13 +54,10 @@ sed -i 's/"QEMU/"'${brand}'/g' hw/input/ps2.c
 sed -i 's/"QEMU Virtio/"'${brand}'/g' hw/input/virtio-input-hid.c
 sed -i 's/QEMU M68K Virtual Machine/'${brand}' M68K Real Machine/g' hw/m68k/virt.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/misc/pvpanic-isa.c
-sed -i 's/aml_append(dev, aml_name_decl("_HID", aml_string("QEMU0001")));/aml_append(dev, aml_name_decl("_HID", aml_string("UEFI0001")));/' hw/misc/pvpanic-isa.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/nvme/ctrl.c
 sed -i 's/0x51454d5520434647ULL/0x4155535520434647ULL/g' hw/nvram/fw_cfg.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/nvram/fw_cfg-acpi.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/pci-host/gpex.c
-sed -i 's/k->vendor_id = PCI_VENDOR_ID_REDHAT;/k->vendor_id = PCI_VENDOR_ID_INTEL;/' hw/pci-host/gpex.c
-sed -i 's/k->device_id = PCI_DEVICE_ID_REDHAT_PCIE_HOST;/k->device_id = PCI_DEVICE_ID_INTEL_P35_MCH;/' hw/pci-host/gpex.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/ppc/prep.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/ppc/e500plat.c
 sed -i 's/qemu-e500/asus-e500/g' hw/ppc/e500plat.c
@@ -84,6 +77,7 @@ sed -i 's/"QEMU/"'${brand}'/g' hw/scsi/spapr_vscsi.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/sd/sd.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/ufs/lu.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/usb/dev-audio.c
+sed -i 's/"QEMU/"'Logitech'/g' hw/usb/dev-hid.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/usb/dev-hub.c
 sed -i 's/314159/114514/g' hw/usb/dev-hub.c
 sed -i 's/"QEMU/"'${brand}'/g' hw/usb/dev-mtp.c
@@ -134,47 +128,16 @@ sed -i 's/"Microsoft Hv/"GenuineIntel/g' target/i386/cpu.c  #解决n卡vgpu驱�
 #sed -i 's/#define PCI_SUBVENDOR_ID_REDHAT_QUMRANET 0x1af4/#define PCI_SUBVENDOR_ID_REDHAT_QUMRANET 0x8086/g' include/hw/pci/pci.h # 0x1afe 是qemu虚拟机的id，这里为了兼容性只处理SUBVENDOR_ID。如果处理了VENDOR_ID=0x1af4 或者 VENDOR_ID=0x1b36 为其他值会造成一些设备无法使用。
 #sed -i 's/#define PCI_VENDOR_ID_REDHAT_QUMRANET    0x1af4/#define PCI_VENDOR_ID_REDHAT_QUMRANET    0x8085/g' include/hw/pci/pci.h #如果处理了VENDOR_ID=0x1af4 或者 VENDOR_ID=0x1b36 为其他值会造成一些设备无法使用。比如scsi virtioNET virtioBlock不认
 #sed -i 's/#define PCI_VENDOR_ID_REDHAT             0x1b36/#define PCI_VENDOR_ID_REDHAT             0x8085/g' include/hw/pci/pci.h #如果处理了VENDOR_ID=0x1af4 或者 VENDOR_ID=0x1b36 为其他值会造成一些设备无法使用。比如scsi virtioNET virtioBlock不认
-sed -i 's/#define PCI_VENDOR_ID_QEMU               0x1234/#define PCI_VENDOR_ID_QEMU               0x8086 \/\/ "???" | Intel Device ID Replacement from 0x1234/' include/hw/pci/pci.h
-sed -i 's/#define PCI_DEVICE_ID_INTEL_82801IR      0x2922/#define PCI_DEVICE_ID_INTEL_82801IR      0x463D \/\/ Intel 82801IR ICH9 - LPC Bridge [A2] | Intel Device ID Replacement from 0x2922 | hw\/ide\/ich.c/' include/hw/pci/pci.h
-sed -i 's/#define PCI_VENDOR_ID_REDHAT_QUMRANET    0x1af4/#define PCI_VENDOR_ID_REDHAT_QUMRANET    0x8086 \/\/ Intel Vendor ID Replacement from 0x1af4/' include/hw/pci/pci.h
-sed -i 's/#define PCI_SUBVENDOR_ID_REDHAT_QUMRANET 0x1af4/#define PCI_SUBVENDOR_ID_REDHAT_QUMRANET 0x8086 \/\/ Intel Subvendor ID Replacement from 0x1af4/' include/hw/pci/pci.h
-sed -i 's/#define PCI_SUBDEVICE_ID_QEMU            0x1100/#define PCI_SUBDEVICE_ID_QEMU            0x8086 \/\/ Intel Subdevice ID Replacement from 0x1100/' include/hw/pci/pci.h
-sed -i 's/#define PCI_VENDOR_ID_REDHAT             0x1b36/#define PCI_VENDOR_ID_REDHAT             0x8086 \/\/ "Red Hat, Inc." | Intel Vendor ID Replacement from 0x1b36/' include/hw/pci/pci.h
-sed -i 's/#define PCI_DEVICE_ID_REDHAT_BRIDGE      0x0001/#define PCI_DEVICE_ID_REDHAT_BRIDGE      0x4641 \/\/ "QEMU PCI-PCI bridge" | Intel Device ID Replacement from 0x0001 | hw\/pci-bridge\/pci_bridge_dev.c/' include/hw/pci/pci.h
-sed -i 's/#define PCI_DEVICE_ID_REDHAT_PCIE_HOST   0x0008/#define PCI_DEVICE_ID_REDHAT_PCIE_HOST   0x4641 \/\/ "QEMU PCIe Host bridge" | Intel Device ID Replacement from 0x0008 | hw\/pci-host\/gpex.c/' include/hw/pci/pci.h
-sed -i 's/#define PCI_DEVICE_ID_REDHAT_PCIE_RP     0x000c/#define PCI_DEVICE_ID_REDHAT_PCIE_RP     0x51b0 \/\/ "QEMU PCIe Root Port" | Intel Device ID Replacement from 0x000c | hw\/pci-bridge\/gen_pcie_root_port.c/' include/hw/pci/pci.h
-sed -i 's/#define PCI_DEVICE_ID_REDHAT_XHCI        0x000d/#define PCI_DEVICE_ID_REDHAT_XHCI        0x51ed \/\/ "QEMU XHCI Host Controller" | Intel Device ID Replacement from 0x000d | hw\/usb\/hcd-xhci-pci.c/' include/hw/pci/pci.h
-sed -i 's/#define PCI_DEVICE_ID_REDHAT_PCIE_BRIDGE 0x000e/#define PCI_DEVICE_ID_REDHAT_PCIE_BRIDGE 0x51b0 \/\/ "Red Hat, Device ID: 000E" | Intel Device ID Replacement from 0x000e | hw\/pci-bridge\/pcie_pci_bridge.c/' include/hw/pci/pci.h
-sed -i 's/#define PCI_DEVICE_ID_INTEL_ICH9_6       0x2930/#define PCI_DEVICE_ID_INTEL_ICH9_6       0x51a3 \/\/ Type: System Device | Category: SMBus Controller | PCI Class: 0x0c05 | hw\/i2c\/smbus_ich9.c/' include/hw/pci/pci_ids.h
-sed -i 's/#define PCI_DEVICE_ID_INTEL_ICH9_8       0x2918/#define PCI_DEVICE_ID_INTEL_ICH9_8       0x5182 \/\/ Type: Bridge Device | Category: LPC Controller   | PCI Class: 0x0601 | hw\/isa\/lpc_ich9.c/' include/hw/pci/pci_ids.h
-sed -i 's/#define PCI_DEVICE_ID_INTEL_P35_MCH      0x29c0/#define PCI_DEVICE_ID_INTEL_P35_MCH      0x4641 \/\/ Type: Bridge Device | Category: Host Bridge | PCI Class: 0x0600 | hw\/pci-host\/q35.c/' include/hw/pci/pci_ids.h
-
-
 sed -i 's/0x1af4/0x10EC/g' hw/audio/hda-codec.c # QEMU_HDA_ID_VENDOR  0x1af4 =ich9-intel-hda
-sed -i 's/k->device_id = 0x2668;/k->device_id = 0x51c8;/' hw/audio/intel-hda.c
-sed -i 's/k->revision = 1;/k->revision = 0;/' hw/audio/intel-hda.c
-sed -i 's/k->device_id = 0x293e;/k->device_id = 0x51c8;/' hw/audio/intel-hda.c
-sed -i 's/k->revision = 3;/k->revision = 0;/' hw/audio/intel-hda.c
-sed -i 's/dc->hotpluggable = true;/dc->hotpluggable = false;/' hw/core/qdev.c
-
+sed -i 's/0x2668/0x51c8/g' hw/audio/intel-hda.c # Intel 82801IB ICH6 - High Definition Audio
+sed -i 's/0x293e/0x51c8/g' hw/audio/intel-hda.c # Intel 82801IB ICH9 - High Definition Audio
 sed -i 's/rev = 3/rev = 4/g' hw/i386/acpi-build.c # Most VMs use an older-style FADT of length 244  bytes (revision  3), cutting off before the Sleep Control/Status registers and Hypervisor ID
 sed -i 's/fadt.rev = 1/fadt.rev = 4/g' hw/i386/acpi-build.c # Most VMs use an older-style FADT of length 244  bytes (revision  3), cutting off before the Sleep Control/Status registers and Hypervisor ID
 sed -i 's/if (f->rev <= 4) {/if (f->rev <= 6) {\n\t\tbuild_append_gas_from_struct(tbl, \&f->sleep_ctl);\n\t\tbuild_append_gas_from_struct(tbl, \&f->sleep_sts);/g' hw/acpi/aml-build.c # # Most VMs use an older-style FADT of length 244  bytes (revision  3), cutting off before the Sleep Control/Status registers and Hypervisor ID
 sed -i 's/lat = 0xfff/lat = 0x1fff/g' hw/i386/acpi-build.c  # A value > 100 indicates the system does not support a C2/C3 state
 sed -i 's/"WAET"/"WWWT"/g' hw/i386/acpi-build.c # "WAET" is also present as a string inside the WAET table, so there's no need to check for its table signature
 sed -i 's/rev = 1/rev = 3/g' hw/i386/acpi-build.c # 全部升级最低为3
-
 sed -i 's/dev = aml_device("PCI0");/aml_append(sb_scope, aml_name_decl("OSYS", aml_int(0x03E8)));\n\tAml *osi = aml_if(aml_equal(aml_call1("_OSI", aml_string("Windows 2012")), aml_int(1)));\n\taml_append(osi, aml_store(aml_int(0x07DC), aml_name("OSYS")));\n\taml_append(sb_scope, osi);\n\tosi = aml_if(aml_equal(aml_call1("_OSI",aml_string("Windows 2013")), aml_int(1)));\n\taml_append(osi, aml_store(aml_int(0x07DD), aml_name("OSYS")));\n\taml_append(sb_scope, osi);\n\taml_append(sb_scope, aml_name_decl("_TZ", aml_int(0x03E8)));\n\taml_append(sb_scope, aml_name_decl("_PTS", aml_int(0x03E8)));\n\tdev = aml_device("PCI0");/g' hw/i386/acpi-build.c # windows 2012 2013 _TZ_ _PTS dsdt
 #sed -i 's/0x0627/0x6666/g' hw/input/virtio-input-hid.c # 0x0627=QEMU tablet usb键鼠会有少量问题
 #sed -i 's/0x0627/0x6666/g' hw/usb/dev-hid.c # 0x0627=QEMU tablet usb键鼠会有少量问题
-sed -i '/^[[:space:]]*fw_cfg_add_acpi_dsdt(sb_scope, x86ms->fw_cfg);$/d' hw/i386/acpi-microvm.c
-sed -i '/^[[:space:]]*fw_cfg_add_acpi_dsdt(sb_scope, x86ms->fw_cfg);$/d' hw/i386/acpi-microvm.c
-sed -i 's/aml_field("PCI0.SF8.PIRQ", AML_BYTE_ACC, AML_NOLOCK, AML_PRESERVE);/aml_field("PCI0.LPCB.PIRQ", AML_BYTE_ACC, AML_NOLOCK, AML_PRESERVE);/' hw/isa/lpc_ich9.c
-sed -i 's/"QEMU"/"Logitech"/g; s/"QEMU USB Mouse"/"USB Mouse"/g; s/"QEMU USB Tablet"/"USB Tablet"/g; s/"QEMU USB Keyboard"/"USB Keyboard"/g; s/"42"/""/g; s/"89126"/""/g; s/"28754"/""/g; s/"68284"/""/g' hw/usb/dev-hid.c
-# 设备描述符中的 Vendor/Product ID
-sed -i 's/\.idVendor          = 0x0627,/.idVendor          = 0x046D,/; s/\.idProduct         = 0x0001,/.idProduct         = 0xC077,/; s/\.idProduct         = 0x0001,/.idProduct         = 0xC31C,/' hw/usb/dev-hid.c
-# product_desc
-sed -i 's/uc->product_desc   = "QEMU USB Tablet";/uc->product_desc   = "USB Tablet";/' hw/usb/dev-hid.c
-sed -i 's/uc->product_desc   = "QEMU USB Mouse";/uc->product_desc   = "USB Mouse";/' hw/usb/dev-hid.c
-sed -i 's/uc->product_desc   = "QEMU USB Keyboard";/uc->product_desc   = "USB Keyboard";/' hw/usb/dev-hid.c
 echo "结束sed工作"
